5a464f4555118ca57ada0e3cef9a40e33b863907
USER <rizkiarya79@yahoo.com>
2024-03-04 16:48:23 +0700

    Jobsheet4 Pertama

---
+++ jobsheet4/variabelkons.php
@@ -0,0 +1,18 @@
+<?php
+$angka1 = 10;
+$angka2 = 5;
+$hasil = $angka1 + $angka2;
+echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil.";
+
+echo"<br>";
+
+$benar = true;
+$salah = false;
+echo "Variabel benar : $benar, Variabel salah : $salah";
+
+echo"<br>";
+
+define("Nama_Situs","Website.com");
+define("Tahun_Pendirian",2023);
+echo "Selamat datang di ".Nama_Situs.", situs yang didirikan pada tahun ".Tahun_Pendirian.".";
+?>
