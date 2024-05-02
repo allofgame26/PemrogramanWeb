062e0d97b0dee9a6500bc9ae0b078fd27008936f
USER <rizkiarya79@yahoo.com>
2024-05-02 23:50:06 +0700

    Koneksi Baru

--- jobsheet10/crud/koneksi.php
+++ jobsheet10/crud/koneksi.php
@@ -1,7 +1,5 @@
 <?php
     $conn = mysqli_connect("localhost","root","","prakwebdb");
-var_dump($conn);
-// die();
     if(mysqli_connect_errno()){
         die("Koneksi database gagal : " .mysqli_connect_error());
     }
